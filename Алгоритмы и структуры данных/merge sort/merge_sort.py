dad277d40c59fe11a6ecebcc90830adadbfd31f5
Никита <60314079+trytofixme@users.noreply.github.com>
2021-02-08 06:23:42 +0300

    Create merge_sort.py

---
+++ Алгоритмы и структуры данных/merge sort/merge_sort.py
@@ -0,0 +1,34 @@
+#p - левая граница, q - середина, r - правая граница
+def Merge(mass, p, q, r):
+    left = mass[p : q + 1]
+    right = mass[q + 1 : r + 1]
+    i, j, k = 0, 0, p
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            mass[k] = left[i]
+            i += 1
+        else:
+            mass[k] = right[j]
+            j += 1
+        k += 1
+    while i < len(left):
+        mass[k] = left[i]
+        i += 1
+        k += 1
+    while j < len(right):
+        mass[k] = right[j]        
+        j += 1
+        k += 1
+    
+
+def MergeSort(mass, p, r):
+    if p < r:
+        q = (p + r) // 2
+        MergeSort(mass, p, q)
+        MergeSort(mass, q + 1, r)
+        Merge(mass, p, q, r)
+        
+mass = [int(ind) for ind in input().split()]
+n = len(mass)
+MergeSort(mass, 0, n - 1)
+print(mass)
